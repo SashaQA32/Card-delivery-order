1f8b5566247ad3cd4238671cbb1062873e862108
Александр Стрюков <sanyajump16@gmail.com>
2022-05-10 19:06:16 +0300

    Added tests

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -1,13 +1,20 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
 
@@ -16,7 +23,7 @@ public class CardDeliveryOrderTest {
         open("http://localhost:9999/");
     }
 
-    
+
     @Test
     public void shouldValidValue() {
         Configuration.timeout = 15000;
@@ -28,5 +35,57 @@ public class CardDeliveryOrderTest {
         $(".checkbox__box").click();
         $(withText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(visible);
+    }
+
+    private void selectPlusSevenDaysDate() {
+        LocalDate today = LocalDate.now();
+
+        LocalDate deliveryDate = LocalDate.now().plusDays(7);
+
+        if (deliveryDate.getMonthValue() != today.getMonthValue()) {
+            $("[type='button']").click();
+            $("[data-step='1']").click();
+        }
+
+        if (deliveryDate.getMonthValue() == today.getMonthValue()) {
+            $("[type='button']").click();
+        }
+    }
+
+    @Test
+    public void shouldSuccessfullyWithManualFilling() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String meetingDate = LocalDate.now().plusDays(3).format(formatter);
+        $("[placeholder='Город']").setValue("Москва");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Виктор Валуев");
+        $("[name='phone']").setValue("+79879879877");
+        $(".checkbox__box").click();
+        $(withText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + meetingDate));
+    }
+
+    @Test
+    public void shouldSuccessfullyWithAutocomplete() {
+
+        String meetingDateDay = String.valueOf(LocalDate.now().plusDays(7).getDayOfMonth());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String meetingDate = LocalDate.now().plusDays(7).format(formatter);
+
+        $("[placeholder='Город']").setValue("Мо");
+        $(byText("Москва")).click();
+        selectPlusSevenDaysDate();
+        $$(".calendar__day").find(text(meetingDateDay)).click();
+        $("[name='name']").setValue("Виктор Валуев");
+        $("[name='phone']").setValue("+79879879877");
+        $(".checkbox__box").click();
+        $(withText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + meetingDate));
     }
 }
